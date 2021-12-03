b3df40a5fd90b0f6b4864c6fc079b7d30b48f9ac
markgol777 <markgol777@gmail.com>
2021-12-03 17:03:48 +0200

    added all files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,49 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
+
 export class AppComponent {
-  title = 'homework1-new';
+  public arrayBadWords: Array<string> = [];
+  public text: string = '';
+  public text2: string = '';
+  public striingBadWords: string = '';
+
+
+  
+  btnAdd() {
+    this.arrayBadWords.push(this.text);
+    console.log(this.arrayBadWords);
+    this.text = '';
+    this.displayElements();
+  }
+
+  displayElements() {
+    let allBadWords = '';
+
+    for (let i = 0; i < this.arrayBadWords.length; i++) {
+      allBadWords += this.arrayBadWords[i] + ', ';
+    }
+
+    this.striingBadWords = allBadWords;
+  }
+
+  resetButton() {
+    this.striingBadWords = '';
+  }
+
+  cenzorButton() {
+      let text = this.text2; 
+      for (let i = 0; i < this.arrayBadWords.length; i++) { 
+          let str1: any = this.arrayBadWords[i];
+          while (text.includes(str1)) { 
+              let str2: string = ''; 
+              for (let j = 0; j < str1.length; j++) { 
+                  str2 += '*'; 
+              } 
+              text = text.replace(str1, str2); 
+          } 
+      } 
+      this.text2 = text; 
+  }
 }
